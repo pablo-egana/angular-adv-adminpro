e1a832fa0551e02067892ef46c5b221d43c452dc
Pablo Egana <pablo.egana.saldundo@gmail.com>
2023-08-22 21:21:21 -0400

    Fin Seccion 3 - Diseño

---
+++ src/app/pages/graph/graph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styles: [
+  ]
+})
+export class GraphComponent {
+
+}
